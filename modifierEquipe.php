7cd61aa2c668b7c022887fae676b83430d737a4f
vallee.romain@gmail.com <vallee.romain@gmail.com>
2024-02-16 14:30:36 +0100

    fix bugs

--- modifierEquipe.php
+++ modifierEquipe.php
@@ -3,7 +3,7 @@ require ('security.php');
 require 'class/equipeDao.class.php';
 
 $equipe = new EquipeDAO();
-var_dump($_POST);
+//var_dump($_POST);
 $equipe->modifierEquipe($_POST['idEquipe'],$_POST['nomEquipe'],$_POST['categorie']);
 
 header("location: ajoutEquipe.php?idTournoi=".$_POST['tournoi_id']);
