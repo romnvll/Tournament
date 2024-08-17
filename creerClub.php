48f6a39227139637d3384585d2632a2ce277a33f
vallee.romain@gmail.com <vallee.romain@gmail.com>
2024-08-17 10:23:23 +0200

    bug creer club

--- creerClub.php
+++ creerClub.php
@@ -2,6 +2,8 @@
 require ('security.php');
 require ('class/clubDao.class.php');
 require 'vendor/autoload.php';
+require 'class/tournoiDao.class.php';
+
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader, [
@@ -13,6 +15,17 @@ $twig->addExtension(new \Twig\Extension\DebugExtension());
 $template = $twig->load('creerClub.twig');
 
 $club = new ClubDAO();
+$tournoiDao = new tournoiDao();
+$tousLesTournois = $tournoiDao->afficherLesTournois();
+
+$dernierId = null;
+
+foreach ($tousLesTournois as $tournoi) {
+    if (isset($tournoi['isArchived']) && $tournoi['isArchived'] == 0) {
+        $dernierId = $tournoi['id'];
+    }
+}
+
 
 
 
@@ -20,6 +33,8 @@ echo $template->render([
   'email' => $_COOKIE['email'],
   'pageEnCours' =>  'GestionClub',
     'ListeDesClubs' => $club->afficherClubs(),
+    'idTournoi' => $dernierId,
+  
 //'ListeDesTournois' => $tournoiDao->afficherLesTournois(),
 //'AfficherClub' => $listeClub->afficherClubs(),
 //'AfficherLesEquipes' => $listeDesEquipes->getAllEquipeByIdTournoi($_GET['idTournoi']),
