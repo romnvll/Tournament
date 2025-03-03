25a71bf6e6e800f6844eb456cf2843aa7ba64718
vallee.romain@gmail.com <vallee.romain@gmail.com>
2025-03-03 14:45:36 +0100

    Ajout de plusieurs equipes par club

--- ajoutEquipe_traitement.php
+++ ajoutEquipe_traitement.php
@@ -1,30 +1,34 @@
 <?php
 require 'security.php';
 
-
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-//require('class/equipe.class.php');
+
 require('class/equipeDao.class.php');
 require ('class/rencontreDao.class.php');
+
 print_r($_POST);
-//$Equipe = new Equipe($_POST['nomEquipe'],$_POST['categorie'],0);
 
-
+// Vérifier si des rencontres existent déjà pour cette catégorie et ce tournoi
 $rencontreDao = new RencontreDAO();
-$RencontreExist = $rencontreDao->rencontresExistByCategorieAndTournoi($_POST['Categorie'],$_POST['IdTournoi']);
+$RencontreExist = $rencontreDao->rencontresExistByCategorieAndTournoi($_POST['Categorie'], $_POST['IdTournoi']);
 
 if ($RencontreExist) {
-    echo "des rencontres existe deja, impossible d'ajouter une equipe<br>Il faut d'abord supprimer les rencontres ";
+    echo "Des rencontres existent déjà, impossible d'ajouter une équipe.<br>Il faut d'abord supprimer les rencontres.";
     exit;
 }
 
-
 $equipeDao = new EquipeDAO();
-$equipeDao->ajouterEquipe($_POST['nomEquipe'],$_POST['Categorie'],$_POST['IdTournoi'],null,$_POST['idClubs']);
+$listeEquipes = preg_split('/[\n,]+/', $_POST['nomEquipes']); // Séparation par virgule et retour à la ligne
 
-header("location: ajoutEquipe.php?idTournoi=".$_POST['IdTournoi'])
+foreach ($listeEquipes as $nomEquipe) {
+    $nomEquipe = trim($nomEquipe); // Supprimer les espaces inutiles
+    if (!empty($nomEquipe)) {
+        $equipeDao->ajouterEquipe($nomEquipe, $_POST['Categorie'], $_POST['IdTournoi'], null, $_POST['idClubs']);
+    }
+}
 
-
-
+// Redirection
+header("Location: ajoutEquipe.php?idTournoi=".$_POST['IdTournoi']);
+exit;
 ?>
